c2e4e65f16c333e9b9bedcac2b334cbfb04bde3b
Scott Grant <scott.grant@thrasher.io>
2018-12-17 11:12:59 +1100

    Handles screen clearing without flickering

--- main.go
+++ main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"encoding/json"
-	"os/exec"
 	"time"
-
-	"github.com/pquerna/otp/totp"
 
 	"fmt"
 	"io/ioutil"
-	"os"
+
+	"github.com/inancgumus/screen"
+	"github.com/pquerna/otp/totp"
 )
 
 const (
 	filePath string = "data.json"
+	format   string = "%-40s%s\n"
 )
 
 func main() {
+	// Clears the screen
+	screen.Clear()
 	for {
-		clearScreen()
+		screen.MoveTopLeft()
 		// Read json and loop through codes
 		entries := readJSONFile(filePath)
 		for _, entry := range entries {
@@ -27,12 +29,15 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
-			fmt.Println(fmt.Sprintf("%v:\t\t%v", entry.Name, code))
+
+			display := fmt.Sprintf(format, entry.Name, code)
+			fmt.Printf("%v", display)
 		}
 
 		// Wait before regenerating codes
 		time.Sleep(time.Second)
 	}
+
 }
 
 // readJSONFile reads a file and converts the JSON to an Entry type
@@ -49,14 +54,6 @@ func readJSONFile(file string) []Entry {
 	}
 
 	return data
-}
-
-// clearScreen runs a clear screen for each time the generation is run
-func clearScreen() {
-	cmd := exec.Command("cmd", "/c", "cls")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-	fmt.Println("\033[2J")
 }
 
 // Entry is an individual 2FA code to generate
